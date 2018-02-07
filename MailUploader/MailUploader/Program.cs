d37c2639ab7d55e46aa6c95755a3df9453e48e1d
KamilBugno <bugno@student.agh.edu.pl>
2018-02-07 14:54:12 +0100

    add initial project

---
+++ MailUploader/MailUploader/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TikaOnDotNet.TextExtraction;
+
+namespace MailUploader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var textExtractor = new TextExtractor();
+
+            var wordDocContents = textExtractor.Extract(@"C:\Users\Kamil\Desktop\sonetyAdamaMickiewicza.pdf");
+
+            Console.WriteLine(wordDocContents.Text);
+            Console.ReadKey();
+        }
+    }
+}
